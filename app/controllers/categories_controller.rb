6eee184f8e276a7d9b7f9a509c489e790ebe8d7f
Shuveksha Tuladhar <tuladhar.shuveksha@gmail.com>
2025-08-18 21:53:47 -0700

    Added controller for category

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,54 @@
+class CategoriesController < ApplicationController
+  before_action :require_login
+  before_action :set_category, only: [ :edit, :update, :destroy, :show ]
+
+  def index
+    @categories = Category.all
+  end
+
+  def show
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      redirect_to categories_path, notice: "Category created successfully."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @category.update(category_params)
+      redirect_to categories_path, notice: "Category updated successfully."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @category.destroy
+    redirect_to categories_path, notice: "Category deleted successfully."
+  end
+
+  private
+
+  def set_category
+    @category = Category.find(params[:id])
+  end
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
+
+  def require_login
+    redirect_to login_path, alert: "Please log in." unless current_user
+  end
+end
