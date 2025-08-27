04d79475e188f467654b14756883350cdb6bd7a9
Sisi Wang <110059102+Sisi-tech@users.noreply.github.com>
2025-08-26 20:27:05 -0400

    fix navbar profile pic to upload and change profile pic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
   include SessionsHelper
+
+  def current_user 
+    @current_user ||= User.find_by(id: session[:user_id])
+  end 
 end
