99dd580bf1e136be016ea59d98697b27c75e7624
abraham <abraham@abrahams-MacBook-Air.local>
2025-08-30 16:22:56 -0400

    Payment Model Associations & Validations

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -3,4 +3,6 @@ class Payment < ApplicationRecord
   belongs_to :payer
   belongs_to :payee
   belongs_to :user_group
+
+  validates :owed_amount, :paid_amount, presence: true, numericality: { greater_than: 0 }
 end
