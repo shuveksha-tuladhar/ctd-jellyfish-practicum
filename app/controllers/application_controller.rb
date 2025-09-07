79d75d8c4350cf0cbcdd5211c89c530fa133e35f
Sisi Wang <110059102+Sisi-tech@users.noreply.github.com>
2025-09-07 15:40:53 -0400

    fix duplicate migration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,4 +20,5 @@ class ApplicationController < ActionController::Base
       redirect_to login_path, alert: "You must be logged in to access this page."
     end
   end
+
 end
--- db/migrate/20250831183451_add_user_foreign_keys_to_payments.rb
+++
@@ -1,6 +0,0 @@
-class AddUserForeignKeysToPayments < ActiveRecord::Migration[8.0]
-  def change
-    add_foreign_key :payments, :users, column: :payer_id
-    add_foreign_key :payments, :users, column: :payee_id
-  end
-end
