dde3cb45c027587d00639ed548ccfd892b193a32
Sisi Wang <110059102+Sisi-tech@users.noreply.github.com>
2025-09-08 19:55:47 -0400

    fix error

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -46,19 +46,20 @@ RSpec.configure do |config|
     Rails.root.join('spec/fixtures')
   ]
 
-<<<<<<< HEAD
-<<<<<<< HEAD
+
   config.include AuthHelpers, type: :request
 
   # config.include Devise::Test::IntegrationHelpers, type: :request
-=======
+
   config.include Devise::Test::IntegrationHelpers, type: :request
->>>>>>> 76bc9e2 (try to fix error)
-=======
-  config.include AuthHelpers, type: :request 
-  
+
+  config.include AuthHelpers, type: :request
+
+
+
+
   # config.include Devise::Test::IntegrationHelpers, type: :request
->>>>>>> cbdc3d5 (fix respc)
+
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
