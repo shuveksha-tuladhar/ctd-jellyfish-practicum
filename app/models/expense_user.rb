d1c03d8a3e2be20e77523030ef96adb1b9f844bc
abraham <abraham@abrahams-MacBook-Air.local>
2025-09-09 14:01:18 -0400

    User-Expense_user-expense associations & validation

--- app/models/expense_user.rb
+++ app/models/expense_user.rb
@@ -2,6 +2,9 @@ class ExpenseUser < ApplicationRecord
   belongs_to :user
   belongs_to :expense
 
+<<<<<<< HEAD
 
+=======
+>>>>>>> 3243b1b (User-Expense_user-expense associations & validation)
   validates :user_id, uniqueness: { scope: :expense_id }
 end
