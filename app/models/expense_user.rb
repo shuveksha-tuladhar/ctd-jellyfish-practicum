ffb60222062621fec95910b87e8fbce5383f221c
abraham <abraham@abrahams-MacBook-Air.local>
2025-09-09 11:56:07 -0400

    Create Expense User join table

--- app/models/expense_user.rb
+++ app/models/expense_user.rb
@@ -1,6 +1,9 @@
 class ExpenseUser < ApplicationRecord
   belongs_to :user
   belongs_to :expense
+<<<<<<< HEAD
 
   validates :user_id, uniqueness: { scope: :expense_id }
+=======
+>>>>>>> 3bf7ab0 (Create Expense User join table)
 end
