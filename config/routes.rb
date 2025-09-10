f96d1fa08edfff4a35a4dfea0ebca9609e3692c1
Sisi Wang <110059102+Sisi-tech@users.noreply.github.com>
2025-09-09 22:35:49 -0400

    fix error

--- config/routes.rb
+++ config/routes.rb
@@ -36,9 +36,9 @@ Rails.application.routes.draw do
     resources :balances, only: [ :index ]
   end
 
-  resources :users do 
-    resources :balances, only: [:index]
-  end 
+  resources :users do
+    resources :balances, only: [ :index ]
+  end
 
   get "up" => "rails/health#show", as: :rails_health_check
 
