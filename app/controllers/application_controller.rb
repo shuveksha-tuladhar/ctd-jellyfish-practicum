c38c18e30c20b018467f15a4975602c7d1d31415
Sisi Wang <110059102+Sisi-tech@users.noreply.github.com>
2025-09-09 22:35:49 -0400

    fix error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,14 +2,14 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
   include SessionsHelper
-  helper_method :current_user 
+  helper_method :current_user
 
-  private 
+  private
   def require_login
-    unless current_user 
+    unless current_user
       redirect_to login_path, alert: "Please log in."
-    end 
-  end 
+    end
+  end
 
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
