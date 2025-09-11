c8edcd89ebe559a98a99126efd3f2df3e7c7e15c
Sisi Wang <110059102+Sisi-tech@users.noreply.github.com>
2025-09-10 20:45:04 -0400

    fix error

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema[8.0].define(version: 2025_09_09_200939) do
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -48,15 +46,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_09_09_200939) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "expense_users", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.bigint "expense_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["expense_id"], name: "index_expense_users_on_expense_id"
-    t.index ["user_id"], name: "index_expense_users_on_user_id"
   end
 
   create_table "expenses", force: :cascade do |t|
@@ -128,8 +117,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_09_09_200939) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "expense_users", "expenses"
-  add_foreign_key "expense_users", "users"
   add_foreign_key "expenses", "user_groups"
   add_foreign_key "expenses", "users"
   add_foreign_key "friendships", "users"
