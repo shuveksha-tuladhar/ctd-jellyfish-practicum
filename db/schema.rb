defcebf71affc2545ed8561a7631f02195136f1a
Shuveksha Tuladhar <tuladhar.shuveksha@gmail.com>
2025-09-11 14:28:53 -0700

    JPROR-31 Resolves conflict

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+<<<<<<< HEAD
 
 
 ActiveRecord::Schema[8.0].define(version: 2025_09_09_200939) do
 
 
+=======
+<<<<<<< Updated upstream
+ActiveRecord::Schema[8.0].define(version: 2025_08_27_204303) do
+=======
+ActiveRecord::Schema[8.0].define(version: 2025_09_09_200939) do
+>>>>>>> Stashed changes
+>>>>>>> c141915 (JPROR-31 Resolves conflict)
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
