14ed3d9fd930302def22009ba9a1818df4eea6df
Shuveksha Tuladhar <tuladhar.shuveksha@gmail.com>
2025-09-14 19:30:42 -0700

    updated view with creator's expense and db migration

---
+++ db/migrate/20250915014621_create_expenses_users.rb
@@ -0,0 +1,10 @@
+class CreateExpensesUsers < ActiveRecord::Migration[8.0]
+  def change
+    create_table :expenses_users do |t|
+      t.references :expense, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_09_09_200939) do
+ActiveRecord::Schema[8.0].define(version: 2025_09_15_015724) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -48,6 +48,15 @@ ActiveRecord::Schema[8.0].define(version: 2025_09_09_200939) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "expense_users", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "expense_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["expense_id"], name: "index_expense_users_on_expense_id"
+    t.index ["user_id"], name: "index_expense_users_on_user_id"
+  end
+
   create_table "expenses", force: :cascade do |t|
     t.string "title"
     t.decimal "amount", precision: 10, scale: 2
@@ -57,8 +66,19 @@ ActiveRecord::Schema[8.0].define(version: 2025_09_09_200939) do
     t.datetime "updated_at", null: false
     t.bigint "user_id"
     t.bigint "user_group_id"
+    t.integer "creator_id"
+    t.index ["creator_id"], name: "index_expenses_on_creator_id"
     t.index ["user_group_id"], name: "index_expenses_on_user_group_id"
     t.index ["user_id"], name: "index_expenses_on_user_id"
+  end
+
+  create_table "expenses_users", force: :cascade do |t|
+    t.bigint "expense_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["expense_id"], name: "index_expenses_users_on_expense_id"
+    t.index ["user_id"], name: "index_expenses_users_on_user_id"
   end
 
   create_table "friendships", force: :cascade do |t|
@@ -117,8 +137,12 @@ ActiveRecord::Schema[8.0].define(version: 2025_09_09_200939) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "expense_users", "expenses"
+  add_foreign_key "expense_users", "users"
   add_foreign_key "expenses", "user_groups"
   add_foreign_key "expenses", "users"
+  add_foreign_key "expenses_users", "expenses"
+  add_foreign_key "expenses_users", "users"
   add_foreign_key "friendships", "users"
   add_foreign_key "friendships", "users", column: "friend_id"
   add_foreign_key "group_members", "user_groups"
