c9dd50a477bab380a182b5a8825dc8346f5f1daf
Shuveksha Tuladhar <tuladhar.shuveksha@gmail.com>
2025-09-14 19:28:28 -0700

    Add association in Expense to creator

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,10 +1,23 @@
 class Expense < ApplicationRecord
-  belongs_to :user
+  belongs_to :creator, class_name: "User", foreign_key: "creator_id"
+
   belongs_to :category, optional: true
   belongs_to :user_group, optional: true
+
+  has_many :expenses_users, dependent: :destroy
+  has_many :payors, through: :expenses_users, source: :user
 
   has_many :payments, dependent: :destroy
 
   validates :title, presence: true, length: { maximum: 100 }
   validates :amount, presence: true, numericality: { greater_than: 0 }
+
+  # Set default split type
+  after_initialize :set_default_split_type, if: :new_record?
+
+  private
+
+  def set_default_split_type
+    self.split_type ||= "equal"
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,11 +9,13 @@ class User < ApplicationRecord
     has_many :received_friendships, class_name: "Friendship", foreign_key: :friend_id, dependent: :destroy
     has_many :received_friends, through: :received_friendships, source: :user
 
-
     has_secure_password
     attr_accessor :reset_token
 
-    has_many :expenses, dependent: :nullify
+    has_many :created_expenses, class_name: "Expense", foreign_key: :creator_id, dependent: :destroy
+
+    has_many :expenses_users, dependent: :destroy
+    has_many :expenses, through: :expenses_users
 
     has_many :payments_made, class_name: "Payment", foreign_key: :payer_id, dependent: :destroy
     has_many :payments_received, class_name: "Payment", foreign_key: :payee_id, dependent: :destroy
@@ -48,5 +50,9 @@ class User < ApplicationRecord
         OR (LOWER(last_name) || ' ' || LOWER(first_name)) LIKE :q",
         q: "%#{query}%"
         )
+    end
+
+     def full_name
+        "#{first_name} #{last_name}"
     end
 end
