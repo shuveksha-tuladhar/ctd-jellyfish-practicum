45ee3a63b1ecdeafe9fdd01d01d885d8972a9165
Shuveksha Tuladhar <tuladhar.shuveksha@gmail.com>
2025-09-20 00:14:52 -0700

    Additional fix for lint issues

--- spec/requests/payments_spec.rb
+++ spec/requests/payments_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe PaymentsController, type: :request do
   include Devise::Test::IntegrationHelpers
-  
+
   let!(:payer) do
     User.create!(
       first_name: "Alice",
@@ -38,59 +38,17 @@ RSpec.describe PaymentsController, type: :request do
   end
 
   let!(:expense) do
-
-  let(:creator) do
-    User.create!(first_name: "Creator", last_name: "User", email: "creator@example.com",
-                 password: "password", phone_number: "9999999999")
-
-
-  end
-
-  let!(:payee) do
-    User.create!(
-      first_name: "Bob",
-      last_name: "Jones",
-      email: "bob@example.com",
-      password: "password",
-      phone_number: "7777777777"
-    )
-  end
-
-  let!(:creator) do
-    User.create!(
-      first_name: "Creator",
-      last_name: "User",
-      email: "creator@example.com",
-      password: "password",
-      phone_number: "9999999999"
-    )
-  end
-
-  let!(:group) do
-    UserGroup.create!(name: "Friends", description: "Test group", created_by_user_id: creator.id)
-  end
-
-
-  let(:expense) do
-
-
-    (Controller test payment)
-
-  let!(:expense) do
-
     Expense.create!(title: "Dinner", amount: 100, split_type: "equal", user_id: creator.id, user_group_id: group.id)
   end
 
   let!(:payment) do
-    Payment.create!(expense: expense, payer: payer, payee: payee,
-                    user_group: group, owed_amount: 50, paid_amount: 50)
+    Payment.create!(expense: expense, payer: payer, payee: payee, user_group: group, owed_amount: 50, paid_amount: 50)
   end
 
   before do
     # Stub current_user so views and controllers know who is logged in
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(payer)
   end
-
 
   describe "GET #index" do
     it "returns a success response and lists payments made by the user" do
@@ -101,13 +59,6 @@ RSpec.describe PaymentsController, type: :request do
   end
 
   describe "POST #create" do
-
-    it "renders new when params are invalid" do
-      post user_payments_path(payer), params: { payment: { owed_amount: -10 } }
-
-      expect(response).to have_http_status(:unprocessable_entity)
-      expect(response.body).to include("New Payment")
-
     it "creates a new payment with valid params" do
       post user_payments_path(payer), params: { payment: {
         expense_id: expense.id,
@@ -125,36 +76,22 @@ RSpec.describe PaymentsController, type: :request do
 
     it "renders new when params are invalid" do
       post user_payments_path(payer), params: { payment: { owed_amount: -10 } }
-
       expect(response).to have_http_status(:unprocessable_entity)
-
-      expect(response.body).to include("View All") # ensures _form rendered
-
+      expect(response.body).to include("New Payment")
     end
   end
 
   describe "PATCH #update" do
-
-    it "renders edit when params are invalid" do
-      patch user_payment_path(payer, payment), params: { payment: { owed_amount: -1 } }
-
-      expect(response).to have_http_status(:unprocessable_entity)
-      expect(response.body).to include("Edit Payment")
     it "updates the payment with valid params" do
       patch user_payment_path(payer, payment), params: { payment: { owed_amount: 60 } }
-
       expect(response).to redirect_to(user_payments_path(payer))
       expect(payment.reload.owed_amount).to eq(60)
     end
 
     it "renders edit when params are invalid" do
       patch user_payment_path(payer, payment), params: { payment: { owed_amount: -1 } }
-
       expect(response).to have_http_status(:unprocessable_entity)
-
-
-      expect(response.body).to include("View All") # ensures _form rendered
-
+      expect(response.body).to include("Edit Payment")
     end
   end
 
@@ -163,8 +100,6 @@ RSpec.describe PaymentsController, type: :request do
       expect {
         delete user_payment_path(payer, payment)
       }.to change(Payment, :count).by(-1)
-
-
 
       expect(response).to redirect_to(user_payments_path(payer))
       expect(flash[:alert]).to eq("Deletion Successful")
