61db0fd8f16461a4a826d38717bd9892837fcc91
Shuveksha Tuladhar <tuladhar.shuveksha@gmail.com>
2025-09-22 13:27:39 -0700

    Fix lint error

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,8 @@
 module ApplicationHelper
     def color_for_user(user_id)
       colors = [
-        "#1d917a", "#2a9d8f", "#264653", "#6cae75","#4d908e", "#8ab17d", "#9c89b8", "#577590"
+        "#1d917a", "#2a9d8f", "#264653", "#6cae75", "#4d908e", "#8ab17d", "#9c89b8", "#577590"
       ]
-      colors[user_id % colors.size] 
+      colors[user_id % colors.size]
     end
 end
